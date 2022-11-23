f9dc93e3a80df6cb4b378f2003e78790ac11b9f1
hasan-yamin <hasanyamin12@gmail.com>
2022-11-23 23:20:36 +0300

    data-tp added

--- js/main.js
+++ js/main.js
@@ -1,35 +1,31 @@
-let btns = document.querySelectorAll("#myBtnContainer button");
-// console.log(btns.length);
+let btns = document.querySelectorAll('#myBtnContainer button')
 
-for(let i=0 ; i<btns.length ; i++){
-    btns[i].addEventListener("click",function(){
+btns.forEach((btn) => {
+  btn.addEventListener('click', function () {
+    // remove class active fromm last active
+    var current = document.getElementsByClassName('active')
+    current[0].classList.remove('active')
 
-        // remove class active fromm last active
-        var current = document.getElementsByClassName("active");
-        current[0].classList.remove("active");
+    // get all elements will be filtered
+    let allElm = document.querySelectorAll('.container .filterDiv')
 
-        // get all elements will be filtered
-        let allElm = document.querySelectorAll(".container .filterDiv");
-        // console.log(allElm.length);
-
-        // check type of sellected btn
-        if(btns[i].classList.contains("all")){
-            // add show class to all elements
-            for(let j=0 ; j<allElm.length ; j++){
-                allElm[j].classList.add("show");
-            }
-        }else{
-            // show class will be added to current type of filter
-            for(let j=0 ; j<allElm.length ; j++){
-                if(allElm[j].classList.contains("f-"+btns[i].className)){
-                    allElm[j].classList.add("show");
-                }else{
-                    allElm[j].classList.remove("show");
-                }
-            }
+    // check type of sellected btn
+    if (btn.classList.contains('all')) {
+      // add show class to all elements
+      allElm.forEach((elm) => {
+        elm.classList.add('show')
+      })
+    } else {
+      // show class will be added to current type of filter
+      allElm.forEach((elm) => {
+        if (elm.classList.contains(btn.getAttribute('data-tp'))) {
+          elm.classList.add('show')
+        } else {
+          elm.classList.remove('show')
         }
-        // add class active to current btn
-        btns[i].classList.add("active");
-    });
-}
-
+      })
+    }
+    // add class active to current btn
+    btn.classList.add('active')
+  })
+})
